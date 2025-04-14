9e088e1c7ac2884cf4784c9659012cea92c6f677
Sajani <sajanidhimal1111@gmail.com>
2025-04-14 20:52:29 +0545

    FIX: minor fixes

--- app/(app)/home.tsx
+++ app/(app)/home.tsx
@@ -731,8 +731,7 @@ export default function HomeScreen() {
           
           <TouchableOpacity 
             className="w-12 h-12 bg-blue-500 rounded-lg items-center justify-center"
-            // onPress={addWater}
-             onPress={()=> seedDefaultFoodDatabase()}
+             onPress={addWater}
           >
             <Ionicons name="add" size={32} color="white" />
           </TouchableOpacity>
